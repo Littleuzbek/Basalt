2f39e17ff33bb1f8e0cbf21a7bb0295d0c6c0ab5
Dilshodbek Akbarov <dilshod.akbarov.2003@gmail.com>
2025-02-19 02:08:10 +0500

    update

--- src/components/homeComponents/Team.jsx
+++ src/components/homeComponents/Team.jsx
@@ -1,75 +1,13 @@
-import { FaFacebookF } from "react-icons/fa";
-import { FaLinkedinIn } from "react-icons/fa";
-import { FaXTwitter } from "react-icons/fa6";
-import { IoLogoSkype } from "react-icons/io";
 import teamBack_pic from "../../assets/team-background.jpg";
+import historic from "../../assets/historic.mp4"
 
 export default function Team() {
   return (
     <div className="team" style={{ backgroundImage: `url(${teamBack_pic})` }}>
-      <h2>Our Best Team</h2>
+      <h2>The Company With History</h2>
       <span></span>
       <div>
-        <div className="team-card">
-          <div>
-            <img src="https://sanat.expert-themes.com/wp-content/uploads/2020/10/team3-1-300x300-1-265x300.jpg" loading="lazy" alt="" />
-            <span className="link-tree">
-                <FaFacebookF />
-                <FaLinkedinIn />
-                <FaXTwitter />
-                <IoLogoSkype />
-            </span>
-          </div>
-          <div>
-            <h4>SALLY STAR</h4>
-            <p>Testing Manager</p>
-          </div>
-        </div>
-        <div className="team-card">
-          <div>
-            <img src="https://sanat.expert-themes.com/wp-content/uploads/2020/10/team6-300x300-1-265x300.jpg" loading="lazy" alt="" />
-            <span className="link-tree">
-                <FaFacebookF />
-                <FaLinkedinIn />
-                <FaXTwitter />
-                <IoLogoSkype />
-            </span>
-          </div>
-          <div>
-            <h4>JOHN LEADER</h4>
-            <p>Leading Architect</p>
-          </div>
-        </div>
-        <div className="team-card">
-          <div>
-            <img src="https://sanat.expert-themes.com/wp-content/uploads/2020/10/team7-300x300-1-265x300.jpg" loading="lazy" alt="" />
-            <span className="link-tree">
-                <FaFacebookF />
-                <FaLinkedinIn />
-                <FaXTwitter />
-                <IoLogoSkype />
-            </span>
-          </div>
-          <div>
-            <h4>JOHAN MARTIN</h4>
-            <p>Mechanical Engineering</p>
-          </div>
-        </div>
-        <div className="team-card">
-          <div>
-            <img src="https://sanat.expert-themes.com/wp-content/uploads/2020/10/team8-300x300-1-265x300.jpg" loading="lazy" alt="" />
-            <span className="link-tree">
-                <FaFacebookF />
-                <FaLinkedinIn />
-                <FaXTwitter />
-                <IoLogoSkype />
-            </span>
-          </div>
-          <div>
-            <h4>SHELLY DALTON</h4>
-            <p>Testing Manager</p>
-          </div>
-        </div>
+        <video src={historic} controls preload="metadata"></video>
       </div>
     </div>
   );
