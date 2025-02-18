3a2338117525e40fb22ab2f361e26ef93a23d764
Dilshodbek Akbarov <dilshod.akbarov.2003@gmail.com>
2025-02-19 02:17:45 +0500

    update

--- src/components/homeComponents/Team.jsx
+++ src/components/homeComponents/Team.jsx
@@ -1,9 +1,8 @@
-import teamBack_pic from "../../assets/team-background.jpg";
 import historic from "../../assets/historic.mp4"
 
 export default function Team() {
   return (
-    <div className="team" style={{ backgroundImage: `url(${teamBack_pic})` }}>
+    <div className="team">
       <h2>The Company With History</h2>
       <span></span>
       <div>
